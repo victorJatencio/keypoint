418301b97dff2ff57163115987ce5ca2cddf72ad
victor atencio <victorj.atencio@gmail.com>
2019-02-19 20:48:45 -0500

    Main Nav almost done

---
+++ dist/js/main.js
@@ -0,0 +1,22 @@
+// Main Nav open and close logic
+// $(document).ready(main);
+
+// var counter = 1;
+
+// function main () {
+//   //body...
+//   $('.menu_bar').click(function(){
+//     if(counter == 1) {
+//       $('.main-nav').animate({ height: '0'});
+//       counter = 0;
+//     } else {
+//       counter = 1;
+//       $('.main-nav').animate({ height: '-100%'});
+//     }
+//   });
+
+//   // Show and Hide Submenus
+//   $('.submenu').click(function() {
+//     $(this).children('.sub-children').slideToggle();
+//   });
+// }
