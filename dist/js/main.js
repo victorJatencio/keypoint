447c6226a39c9e9d34452dab00f479a6eb73eaf4
victor atencio <victorj.atencio@gmail.com>
2019-02-22 15:59:40 -0500

    Main Nav animation part one

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,7 @@
-// Main Nav open and close logic
-// $(document).ready(main);
+//Main Nav open and close logic
+$(document).ready(main);
 
-// var counter = 1;
-
-// function main () {
-//   //body...
-//   $('.menu_bar').click(function(){
-//     if(counter == 1) {
-//       $('.main-nav').animate({ height: '0'});
-//       counter = 0;
-//     } else {
-//       counter = 1;
-//       $('.main-nav').animate({ height: '-100%'});
-//     }
-//   });
-
-//   // Show and Hide Submenus
-//   $('.submenu').click(function() {
-//     $(this).children('.sub-children').slideToggle();
-//   });
-// }
+function main() {
+  //body...
+  // Show and Hide Submenus
+}
