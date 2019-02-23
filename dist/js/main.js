7d21ff87f6dbe85d2cf1dbc8462cc8bed7518761
victor atencio <victorj.atencio@gmail.com>
2019-02-23 13:11:29 -0500

    Main Nav ready

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,28 @@
 //Main Nav open and close logic
-$(document).ready(main);
+$(document).ready(function() {
+  let $screen = $(window);
 
-function main() {
-  //body...
-  $(".menu_bar").click(function() {
-    $(".main-nav ul:first").toggleClass("open-menu");
-  });
-  // Show and Hide Submenus
-  $(".submenu").click(function() {
-    $(this)
-      .children(".sub-children")
-      .slideToggle();
-  });
-}
+  function mainNav() {
+    //body...
+    $(".menu_bar").click(function() {
+      $(".main-nav ul:first").toggleClass("open-menu");
+    });
+
+    // Show and Hide Submenus
+    $(".submenu").on("click", function(e) {
+      e.preventDefault();
+
+      let screenSize = $screen.width();
+
+      // Optimize for screen sizes
+      if (screenSize >= 320 && screenSize <= 850) {
+        $(this)
+          .children(".sub-children")
+          .slideToggle();
+      } else if (screenSize >= 851) {
+        return false;
+      }
+    });
+  }
+  mainNav();
+});
