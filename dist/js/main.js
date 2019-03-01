bc18fed21bda4451abf06d9b0f441a8f5b35ba2a
victor atencio <victorj.atencio@gmail.com>
2019-03-01 15:43:18 -0500

    Stiky bar and Search overlay done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,41 @@
+//Select DOM Items
+const formReset = document.getElementById("search-field");
+const searchIcon = document.querySelector(".btn--search");
+const searchOverlay = document.querySelector(".top-search-wrapper");
+const closeOverlay = document.querySelector(".btn--search-close");
+
+// Set Initial state of search overlay
+let showSearch = false;
+
+searchIcon.addEventListener("click", openSearch);
+closeOverlay.addEventListener("click", openSearch);
+
+function openSearch() {
+  document.getElementById("search-field").reset();
+  if (!showSearch) {
+    searchOverlay.classList.add("show-search");
+    // Set Search State
+    showSearch = true;
+  } else {
+    searchOverlay.classList.remove("show-search");
+    // Set Search State
+    showSearch = false;
+  }
+}
+
+//Stiky Nav
+const stikyNav = document.querySelector("#nav");
+const topOfNav = stikyNav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+  }
+}
+window.addEventListener("scroll", fixNav);
+
 //Main Nav open and close logic
 $(document).ready(function() {
   let $screen = $(window);
